4624eee0cffdcc7beac7de9e9cc575e5cbcb240a
Michael-Kennedy-AiO <michael.gamestudio@gmail.com>
2021-10-04 07:27:48 +0700

    Cap Nhap tuan 4

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,7 @@
 package main;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
 }
